32fab4a548793e0ba7b8d5624d511f06b6196c1b
sreepooj <poojakarapureddy2297>
2018-03-10 11:18:47 +0530

    hiii

--- script/main.js
+++ script/main.js
@@ -13,16 +13,16 @@ xhr.send();
 getfile("resources/data.json", function(text){
 let data=JSON.parse(text);
 console.log(data);
-// career(data.Career);
+career(data.career);
 })
-// var right_child=document.querySelector(".content_child");
-// function career(info){
-// var h2=document.createElement("h2");
-// h2.textContent="Career Objective";
-// right_child.appendChild(h2);
-// var hr=document.createElement("hr");
-// right_child.appendChild(hr);
-// var career_info=document.createElement("p");
-// career_info.textContent=info.objective;
-// right_child.appendChild(career_info);
-// }
+var right_child=document.querySelector(".content_child");
+function career(info){
+var h2=document.createElement("h2");
+h2.textContent="Career Objective";
+right_child.appendChild(h2);
+var hr=document.createElement("hr");
+right_child.appendChild(hr);
+var career_info=document.createElement("p");
+career_info.textContent=info.objective;
+right_child.appendChild(career_info);
+}
